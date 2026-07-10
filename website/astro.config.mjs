0706a5b1564f24c986c750636edf9676b9f2e6e4
Amr Loksha <amrloksha151@proton.me>
2026-07-10 13:20:18 +0300

    Enhancing user experience

--- website/astro.config.mjs
+++ website/astro.config.mjs
@@ -3,6 +3,9 @@ import { defineConfig } from 'astro/config';
 import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
+  prefetch: {
+    prefetchAll: true,
+  },
   vite: {
     plugins: [tailwindcss()],
   },
